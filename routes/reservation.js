13931d499a6706eac2e83d51bee7c283b23273d6
Athanase Kpan <athanasesura1@gmail.com>
2025-07-06 20:13:44 -0400

    ajoute des function pour obtenir la liste des reservation d'un utilisateur connecté aussi modifier ou supprimer

--- routes/reservation.js
+++ routes/reservation.js
@@ -3,7 +3,7 @@ const router = express.Router()
 import {modifier, reserver, supprimer, trouver} from "../controllers/reservationController.js";
 router.post("/reservation", reserver)
 router.delete("/reservation/suppression", supprimer)
-router.post("reservation/modifier", modifier)
+router.post("/reservation/modifier", modifier)
 router.get("/reservations", trouver)
 
 export default  router;
